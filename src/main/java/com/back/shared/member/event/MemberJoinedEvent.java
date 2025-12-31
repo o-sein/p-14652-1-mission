719903e97d43b18422f5f525bf36edcce1f0fd15
ooooo_ori <dhtp0112@gmail.com>
2025-12-31 14:38:57 +0900

    0003 : 앱 시작시, MemberJoinEvent 이벤트 발행

---
+++ src/main/java/com/back/shared/member/event/MemberJoinedEvent.java
@@ -0,0 +1,9 @@
+package com.back.shared.member.event;
+
+import com.back.standard.event.HasEventName;
+import com.back.shared.member.dto.MemberDto;
+
+public record MemberJoinedEvent(
+        MemberDto member
+) implements HasEventName {
+}
